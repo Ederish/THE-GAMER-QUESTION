bc9ddf836ab3624691964594d9634a474dbb0e17
Ederish <63983547+Ederish@users.noreply.github.com>
2021-12-03 17:38:57 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const CACHE_NAME = 'v1_cache_programador_fitness',
     './style.css',
     './script.js',
     './img/logo.png',
-    './img/fondo6.png',
+    './img/fondo6.jpeg',
     './img/favicon.png'
   ]
 
